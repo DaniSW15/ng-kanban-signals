d81debe7c28d6705d66ee8a295ec1c1712c2b333
Daniel CG <daniel.cg15.tab@gmail.com>
2026-05-22 00:27:32 -0600

    Auto-generated commit

---
+++ chunk-DV6F4ZUI.js
@@ -0,0 +1,1 @@
+import"./chunk-2NFLSA4Y.js";var e=[{path:"",loadChildren:()=>import("./chunk-FC53MZU2.js").then(t=>t.kanbanRoutes)},{path:"**",redirectTo:"",pathMatch:"full"}];export{e as default};
--- chunk-FQRU5TKB.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-2NFLSA4Y.js";var e=[{path:"",loadChildren:()=>import("./chunk-AENRQXQJ.js").then(t=>t.kanbanRoutes)},{path:"**",redirectTo:"",pathMatch:"full"}];export{e as default};
